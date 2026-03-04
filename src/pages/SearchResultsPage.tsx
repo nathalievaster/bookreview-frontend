d87054a69f743010de94d511e97eae22d238a6af
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-04 01:49:30 +0100

    påbörjat routing

--- src/pages/SearchResultsPage.tsx
+++ src/pages/SearchResultsPage.tsx
@@ -1,1 +1,10 @@
 // Visa sökresultat från Google Books API
+import React from 'react'
+
+const SearchResultsPage = () => {
+  return (
+    <div>SearchResultsPage</div>
+  )
+}
+
+export default SearchResultsPage
---
+++ src/routing.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";¨
+import BookDetailsPage from "./pages/BookDetailsPage";
+import LoginPage from "./pages/LoginPage";
+import MyPage from "./pages/MyPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import RegisterPage from "./pages/RegisterPage";
+import SearchResultsPage from "./pages/SearchResultsPage";
+
+cons router = createBrowserRouter([
+    
+])
