384b1691bbdfb430610d7324921b614d7f1ea35a
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-04 12:37:53 +0100

    notfound-sida

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
-const NotFoundPage = () => {
+function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <div>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+      <p>The page you are looking for does not exist.</p>
+
+      <Link to="/">Go back to Home</Link>
+    </div>
   )
 }
 
