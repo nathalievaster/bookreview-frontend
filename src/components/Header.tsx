a9a0b9cea383e3294e86a9d5dcfaaaef2604cd0d
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-06 05:19:21 +0100

    justeringar i header och global css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,13 +9,41 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <ul className={styles.navList}>
-        <li><NavLink to="/" className={styles.navLink}>Startsida</NavLink></li>
-        <li><NavLink to="/mypage" className={styles.navLink}>Mina sidor</NavLink></li>
+
         <li>
-          {
-            !user ? <NavLink to="/login" className={styles.navLink}>Logga in</NavLink> : <button onClick={logout} className={styles.logoutButton}>Logga ut</button>
-          }
+          <NavLink to="/" className={styles.navLink}>
+            Startsida
+          </NavLink>
         </li>
+
+        {user && (
+          <li>
+            <NavLink to="/mypage" className={styles.navLink}>
+              Mina sidor
+            </NavLink>
+          </li>
+        )}
+
+        {user?.role === "admin" && (
+          <li>
+            <NavLink to="/admin" className={styles.navLink}>
+              Admin
+            </NavLink>
+          </li>
+        )}
+
+        <li>
+          {!user ? (
+            <NavLink to="/login" className={styles.navLink}>
+              Logga in
+            </NavLink>
+          ) : (
+            <button onClick={logout} className={styles.logoutButton}>
+              Logga ut
+            </button>
+          )}
+        </li>
+
       </ul>
     </header>
   )
