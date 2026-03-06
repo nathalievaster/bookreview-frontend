f8c66a7915bc1d97b388c11fd53b8b27134a7501
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-06 06:30:53 +0100

    litet utkast på startsidan

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,35 @@
+import styles from "./css/Home.module.css"
+import bookshelf from "../assets/bookshelf.avif"
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <section className={styles.section}>
+        <img src={bookshelf} alt="Bokhylla" />
+        <div>
+          <h1>Välkommen, alla boktokar, till Boktok!</h1>
+          <p>
+            Har du svårt att komma på vilken bok du ska dyka ner i härnäst?
+            Eller vill du läsa andras tankar om boken du precis läst klart?
+            Här på Boktok kan du, utan eget konto, söka bland böcker
+            och läsa andra användares recensioner.
+          </p>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+
+
+        <div>
+          <h2>Vill du bidra med egna recensioner?</h2>
+          <p>
+            För att dela dina egna tankar om böcker du läst gör du det enkelt
+            genom att logga in HÄR. Har du inget konto? Ingen fara!
+            Du registrerar dig snabbt och smidigt HÄR.
+          </p>
+        </div>
+      </section>
+    </>
   )
 }
 
