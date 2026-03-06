690e5598f9fa2f6e19543041f89e6d12aa4642e6
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-06 05:03:56 +0100

    layout-fil med footer och header

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+        <p>Boktok AB</p>
+        <p>Biblioteksgatan 5a</p>
+        <p>Bästkusten</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,17 @@
+import { Outlet } from "react-router-dom"
+import Header from "./Header"
+import Footer from "./Footer"
+
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+
+    </>
   )
 }
 
