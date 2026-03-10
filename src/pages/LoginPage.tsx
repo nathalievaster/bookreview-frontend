16b156b8a8e74cc13988b2fc61163aae4de8eb41
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-10 13:25:01 +0100

    utkast till logga in

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,47 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
+import styles from "./css/LoginPage.module.css"
+
 const LoginPage = () => {
+
+  const { login } = useAuth()
+  const navigate = useNavigate()
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    try {
+      await login({ username, password })
+      navigate("/")
+    } catch {
+      setError("Fel användarnamn eller lösenord")
+    }
+  }
+
   return (
-    <div>LoginPage</div>
+    <div className={styles.container}>
+
+      <h1>Logga in</h1>
+
+      <form onSubmit={handleSubmit} className={styles.form}>
+
+        <input type="text" placeholder="Användarnamn" value={username} onChange={(e) => setUsername(e.target.value)}required/>
+
+        <input type="password" placeholder="Lösenord" value={password} onChange={(e) => setPassword(e.target.value)} required />
+
+        <button className="add-btn" type="submit">Logga in</button>
+        <p>Inget konto än?</p>
+        <button className="add-btn">Registrera dig här</button>
+
+      </form>
+
+      {error && ( <p className={styles.error}> {error}</p> )}
+    </div>
   )
 }
 
