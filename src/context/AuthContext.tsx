6e1b9e422c31e9cb3b17049e815aa29176604680
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-10 15:35:36 +0100

    användaren loggas ej ut vid uppdatering

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -11,6 +11,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     
     // State för att hålla reda på den inloggade användaren
     const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
 
     // Logga in användaren genom att skicka credentials till backend och spara token o user i state
     const login = async (credentials: LoginCredentials) => {
@@ -23,12 +24,16 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
                 },
                 body: JSON.stringify(credentials)
             });
+
             if (!res.ok) {
                 throw new Error("Inloggningen misslyckades.");
             }
+
             const data = await res.json() as AuthResponse;
+
             localStorage.setItem("token", data.token);
             setUser(data.user);
+
         } catch (error) {
             throw error;
         }
@@ -42,8 +47,11 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
     // Validera token för att behålla användarens inloggning även efter uppdatering av sidan
     const validateToken = async () => {
+
         const token = localStorage.getItem("token");
+
         if (!token) {
+            setLoading(false);
             return;
         }
 
@@ -54,32 +62,44 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
                     "Authorization": `Bearer ${token}`
                 }
             });
+
             if (!res.ok) {
                 throw new Error("Token är ogiltig.");
             }
+
             const data = await res.json() as User;
+
             setUser(data);
+
         } catch (error) {
             localStorage.removeItem("token");
             setUser(null);
         }
+
+        setLoading(false);
     }
 
     useEffect(() => {
         validateToken();
     }, []);
 
+    if (loading) {
+        return <p>Laddar...</p>;
+    }
+
     return (
-        <AuthContext.Provider value ={{ user, login, logout }}>
+        <AuthContext.Provider value={{ user, login, logout }}>
             {children}
         </AuthContext.Provider>
     )
 }
 
-export const useAuth = () : AuthContextType=> {
+export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext);
+
     if (!context) {
         throw new Error("useAuth måste användas inom en AuthProvider");
     }
+
     return context;
 }
