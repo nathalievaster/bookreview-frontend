38061d07ababb696f0550ceed71fb104eee1863e
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-11 11:37:23 +0100

    admin-sida kan hämta alla recensioner

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,6 +1,92 @@
+import { useEffect, useState } from "react"
+import styles from "./css/AdminPage.module.css"
+import type { Review } from "../types/review.types"
+
 const AdminPage = () => {
+
+  const [reviews, setReviews] = useState<Review[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetchReviews()
+  }, [])
+
+  const fetchReviews = async () => {
+
+    const token = localStorage.getItem("token")
+
+    const res = await fetch("http://localhost:5000/api/reviews", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    const data = await res.json()
+
+    setReviews(data)
+    setLoading(false)
+  }
+
+  const deleteReview = async (id: string) => {
+
+    const token = localStorage.getItem("token")
+
+    await fetch(`http://localhost:5000/api/reviews/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    setReviews(reviews.filter(review => review._id !== id))
+  }
+
+  if (loading) {
+    return <p>Laddar recensioner...</p>
+  }
+
   return (
-    <div>AdminPage</div>
+    <div className={styles.container}>
+
+      <h1>Adminpanel</h1>
+
+      <h2>Alla recensioner</h2>
+
+      {reviews.length === 0 && (
+        <p>Inga recensioner finns.</p>
+      )}
+
+      <section className={styles.reviewList}>
+
+        {reviews.map(review => (
+
+          <div key={review._id} className={styles.reviewCard}>
+
+            <div className={styles.reviewHeader}>
+
+              <span className={styles.user}>
+                {review.user.username}
+              </span>
+
+              <span className={styles.rating}>
+                ⭐ {review.rating}/5
+              </span>
+
+            </div>
+
+            <p className={styles.text}>
+              {review.reviewText}
+            </p>
+
+            <button className="delete-btn" onClick={() => deleteReview(review._id)}>Ta bort recension</button>
+
+          </div>
+
+        ))}
+
+      </section>
+
+    </div>
   )
 }
 
