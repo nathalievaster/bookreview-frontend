c5ed9c7bf5597cab777c63d804c5c6cfb5fd4a6f
Nathalie Fredriksson <nfredriksson1@gmail.com>
2026-03-18 08:35:11 +0100

    ny footer med installation av react icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,36 @@
 import styles from "./Footer.module.css"
-
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <footer>
-        <p>Boktok AB</p>
-        <p>Biblioteksgatan 5a</p>
-        <p>Bästkusten</p>
+    <footer className={styles.footer}>
+      <div className={styles.logo}>Boktok</div>
+
+      <div className={styles.columns}>
+        <div className={styles.column}>
+          <h4>Besök oss</h4>
+          <p>Biblioteksgatan 5a</p>
+          <p>Bästkusten</p>
+        </div>
+
+        <div className={styles.column}>
+          <h4>Utforska</h4>
+          <p>Sök böcker</p>
+          <p>Min läslista</p>
+          <p>Mina recensioner</p>
+        </div>
+
+        <div className={styles.column}>
+          <h4>Följ oss</h4>
+          <p><FaFacebook /> Facebook</p>
+          <p><FaInstagram /> Instagram</p>
+          <p><FaTwitter /> Twitter</p>
+        </div>
+      </div>
+
+      <div className={styles.bottom}>
+        <p>2026 Boktok AB – Läsning är livet</p>
+      </div>
     </footer>
   )
 }
